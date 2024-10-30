37587b99975f7259639f725567bf7531f3cc42c1
Kihoon Han <kihoon.han@furiosa.ai>
2024-10-30 14:56:13 +0900

    Add `NodeName` property at `Config`, and add default `NodeName` creation logic

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,32 @@
 package config
 
+import "os"
+
 type Config struct {
-	Port     int `yaml:"port"`
-	Interval int `yaml:"interval"`
+	Port     int    `yaml:"port"`
+	Interval int    `yaml:"interval"`
+	NodeName string `yaml:"nodeName"`
 }
 
 func NewDefaultConfig() *Config {
 	return &Config{
 		Port:     6254,
 		Interval: 10,
+
+		// Set NodeName from `NODE_NAME` env. If not set, try it with OS HostName.
+		NodeName: func() string {
+			var nodeName string
+			var err error
+
+			nodeName = os.Getenv("NODE_NAME")
+			if nodeName == "" {
+				nodeName, err = os.Hostname()
+				if err != nil {
+					panic(err)
+				}
+			}
+
+			return nodeName
+		}(),
 	}
 }
